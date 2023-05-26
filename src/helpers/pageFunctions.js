7c62b8a20fc33aa703f1bcd239b90e07a25a8e60
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-26 09:21:36 -0300

    Continuando requisito 2

--- src/helpers/pageFunctions.js
+++ src/helpers/pageFunctions.js
@@ -1,4 +1,4 @@
-import { searchCities } from './weatherAPI';
+import { searchCities, getWeatherByCity } from './weatherAPI';
 
 /**
  * Cria um elemento HTML com as informações passadas
@@ -110,12 +110,17 @@ export function createCityElement(cityInfo) {
 /**
  * Lida com o evento de submit do formulário de busca
  */
-export function handleSearch(event) {
+export async function handleSearch(event) {
   event.preventDefault();
   clearChildrenById('cities');
-
   const searchInput = document.getElementById('search-input');
   const searchValue = searchInput.value;
   searchCities(searchValue);
-  // seu código aqui
+  const cities = await searchCities(searchValue);
+  const cityPromises = await cities.map((city) => getWeatherByCity(city.url));
+  const cityInfo = await Promise.all(cityPromises);
+  cityInfo.forEach((city) => {
+    citiesList(city);
+    searchButton(city.url);
+  });
 }
--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,6 @@
 // Remova os comentários a medida que for implementando as funções
 
 export const searchCities = async (term) => {
-  //   seu código aqui
   const TOKEN = import.meta.env.VITE_TOKEN;
   const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
   const data = await response.json();
@@ -12,23 +11,17 @@ export const searchCities = async (term) => {
   return data;
 };
 
-export const getWeatherByCity = (cityURL) => {
-  //   seu código aqui
-  try {
-    const TOKEN = import.meta.env.VITE_TOKEN;
-    const response = fetch(`http://api.weatherapi.com/v1/forecast.json?lang=pt&key=${TOKEN}&q=${cityURL}`);
-    const data = response.json();
-    const {
-      temp: temperature,
-      condition: { text: conditionText, icon: conditionIcon },
-    } = data.current;
-    return {
-      temperature,
-      conditionText,
-      conditionIcon,
-    };
-  } catch (error) {
-    console.error('Ocorreu um erro ao obter as infos do tempo para as cidades:', error);
-    return null;
-  }
+export const getWeatherByCity = async (cityURL) => {
+  const TOKEN = import.meta.env.VITE_TOKEN;
+  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?lang=pt&key=${TOKEN}&q=${cityURL}`);
+  const data = await response.json();
+  const {
+    temp: temperature,
+    condition: { text: conditionText, icon: conditionIcon },
+  } = data.current;
+  return {
+    temperature,
+    conditionText,
+    conditionIcon,
+  };
 };
