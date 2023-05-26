6c9dac50e1fb769e9ad868377ed294b94c8dcdd2
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-26 19:28:55 -0300

    Requisito 2 finalizado corrigido o erro no link

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,18 +1,25 @@
-// Remova os comentários a medida que for implementando as funções
 const TOKEN = import.meta.env.VITE_TOKEN;
 
 export const searchCities = async (term) => {
-  const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
-  const data = await response.json();
-  if (data.length === 0) {
-    window.alert('Nenhuma cidade encontrada');
+  const urlAPI = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
+
+  try {
+    const response = await fetch(urlAPI);
+    const data = await response.json();
+    if (data.length === 0) {
+      window.alert('Nenhuma cidade encontrada');
+    }
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
     return [];
   }
-  return data;
 };
 
 export const getWeatherByCity = async (cityURL) => {
-  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?lang=pt&key=${TOKEN}&q=${cityURL}`);
+  const urlAPI = `http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`;
+
+  const response = await fetch(urlAPI);
   const data = await response.json();
   return {
     temp: data.current.temp_c,
